368d1473e491276cbb18726f010903c8cfdd2a33
Darshan Darshu <darshan15aj@gmail.com>
2021-06-05 22:16:28 +0530

    scraping full

---
+++ webgreeks4.js
@@ -0,0 +1,28 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const json2csv = require("json2csv").Parser;
+
+const WriteStream = fs.createWriteStream("webgreeks.csv");
+const webGreeks =
+  "http://webgeeks-3.herokuapp.com/web4.html";
+(async () => {
+  {
+    const response = await request({
+      uri: webGreeks,
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language":
+          "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
+      },
+      // json: true,
+    });
+
+    let $ = cheerio.load(response);
+    // Title
+    let html = $("html");
+    console.log(html);
+  }
+})();
